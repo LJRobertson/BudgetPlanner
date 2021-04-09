7f32e3d8f435baa7fd7e3d2f2a8339b204fc5152
unknown <lindseyjrobertson@gmail.com>
2021-04-08 21:18:12 -0400

    Initial Commit

---
+++ BudgetPlanner.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BudgetPlanner.WebMVC.MvcApplication" Language="C#" %>
---
+++ BudgetPlanner.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BudgetPlanner.WebMVC.Startup))]
+namespace BudgetPlanner.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
